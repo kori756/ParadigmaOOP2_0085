abcfb8f3cd28b560b5eea2713713f274933387ac
kori756 <khorirachmatr@gmail.com>
2026-05-19 19:28:44 +0700

    menambahkan class pekerja

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -13,3 +13,13 @@ public:
     }
 };
 
+class pekerja : virtual public orang
+{
+public:
+    pekerja(int pUmur) : orang(pUmur)
+    {
+        cout << "Pekerja dibuat\n"
+             << endl;
+    }
+};
+
