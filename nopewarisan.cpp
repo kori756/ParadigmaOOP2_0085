041484873528ab1c510b11ff7b5e56d896ae05f7
kori756 <khorirachmatr@gmail.com>
2026-05-19 19:43:03 +0700

    menambahkan class baseClass

--- nopewarisan.cpp
+++ nopewarisan.cpp
@@ -1,3 +1,10 @@
 #include <iostream>
 using namespace std;
 
+class baseClass final{
+  public:
+        virtual void perkenalan(){
+          cout << "hallo saya function dari base class";
+        }
+};
+
