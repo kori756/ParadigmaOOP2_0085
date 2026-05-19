2dfb9f2603a33ee91938a8047c82fcd51f11afee
kori756 <khorirachmatr@gmail.com>
2026-05-19 19:19:14 +0700

    menyelesaikan class orang

--- solvemultiwaris.cpp
+++ solvemultiwaris.cpp
@@ -3,3 +3,13 @@ using namespace std;
 
 class orang
 {
+public:
+    int umur;
+
+    orang(int pUmur) : umur(pUmur)
+    {
+        cout << "Orang dibuat dengan umur " << umur << "\n"
+             << endl;
+    }
+};
+
